9cb9c4a12239320c876cd0d77f5acc4311272666
Kiyu <qconnection@gmail.com>
2015-01-31 22:13:56 +0100

    Added edit action for Crimes controller

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,5 +1,24 @@
 class CrimesController < ApplicationController
 	def new
-		@game_id = params[:game_id]
+		crime = Crime.all.sample
+		redirect_to edit_game_crime_path(params[:game_id], crime.id)
+	end
+
+	def edit
+		false_leads_number = 2
+		@crimes_list = (1..Crime.all.count).to_a
+		@artists = Array.new
+
+		artist = Crime.find(params[:id]).artists.sample
+		@crimes_list.delete(artist.crime_id)
+		@artists.push(artist)
+
+
+		false_leads_number.times do
+			random_crime = @crimes_list.sample
+			artist = Crime.find(random_crime).artists.sample
+			@crimes_list.delete(random_crime)
+			@artists.push(artist)
+		end
 	end
 end
